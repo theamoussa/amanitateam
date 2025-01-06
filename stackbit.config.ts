2ba4f460eae31972a1bb60bb1012ab24463c79db
theamoussa <141860341+theamoussa@users.noreply.github.com>
2025-01-06 13:00:39 +0000

    -am

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -3,20 +3,14 @@ import { defineStackbitConfig } from '@stackbit/types';
 export default defineStackbitConfig({
   stackbitVersion: '~0.5.0',
 
-  // Content sources
   contentSources: [
     {
-      name: 'GitHub Repository',
-      fetchFrom: {
-        provider: 'github',
-        owner: 'theamoussa', // GitHub username
-        repo: 'amanitateam', // Repository name
-        branch: 'main', // Branch to use
-      },
+      repoUrl: 'https://github.com/theamoussa/amanitateam',
+      branch: 'main',
     },
   ],
 
-  devCommand: 'npm run dev', // Adjust based on your project
-  buildCommand: 'npm run build', // Optional: Specify your build command
-  outputDirectory: 'dist', // Optional: Specify your output directory
+  devCommand: 'npm run dev',
+  buildCommand: 'npm run build',
+  outputDirectory: 'dist',
 });
